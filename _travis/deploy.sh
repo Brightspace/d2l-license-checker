007f1a7d790bf4818d4fc27f26ca4e16264870c9
Jeff Kehres <Jeff.Kehres@D2L.com>
2017-01-17 15:30:36 -0500

    Fixed deployment script

--- _travis/deploy.sh
+++ _travis/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 ARTIFACTORY_REGISTRY=https://d2lartifacts.artifactoryonline.com/d2lartifacts/api/npm/npm-local
 
-echo "Running deploy script"
-echo "TEMP: $ARTIFACTORY_USER:$ARTIFACTORY_PASS"
-
-curl -ks -u$ARTIFACTORY_USER:$ARTIFACTORY_PASS "${ARTIFACTORY_REGISTRY}/auth/d2l" -o ~/.npmrc
+curl -ks -u$ARTIFACTORY_USER:$ARTIFACTORY_PASS "${ARTIFACTORY_REGISTRY}/auth/d2l" -o .npmrc
 npm config set @d2l:registry $ARTIFACTORY_REGISTRY
-#yes '' | npm adduser
 npm publish --registry $ARTIFACTORY_REGISTRY
