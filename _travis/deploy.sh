69a3df99777372dc76c893cc492fdeeb8631994f
Christian Zommerfelds <Christian.Zommerfelds@D2L.com>
2017-01-17 12:57:27 -0500

    trying travis deployment

--- _travis/deploy.sh
+++ _travis/deploy.sh
@@ -4,7 +4,7 @@ ARTIFACTORY_REGISTRY=https://d2lartifacts.artifactoryonline.com/d2lartifacts/api
 echo "Running deploy script"
 echo "TEMP: $ARTIFACTORY_USER:$ARTIFACTORY_PASS"
 
-curl -ks -u$ARTIFACTORY_USER:$ARTIFACTORY_PASS "${ARTIFACTORY_REGISTRY}/auth/d2l" -o .npmrc
+curl -ks -u$ARTIFACTORY_USER:$ARTIFACTORY_PASS "${ARTIFACTORY_REGISTRY}/auth/d2l" -o ~/.npmrc
 npm config set @d2l:registry $ARTIFACTORY_REGISTRY
-yes '' | npm adduser
+#yes '' | npm adduser
 npm publish --registry $ARTIFACTORY_REGISTRY
