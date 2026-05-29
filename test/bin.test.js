9f20271eec6b18e7bbf8bee9e95485ea3dadaac0
Devon Powell <Devon.Powell@D2L.com>
2026-05-29 10:17:54 -0400

    Modernize test suite

--- test/bin.test.js
+++ test/bin.test.js
@@ -1,23 +1,25 @@
-const assert = require('chai').assert;
-const path = require('path');
-const spawnSync = require('child_process').spawnSync;
-const _ = require('lodash');
+const { expect } = require('chai');
+const { join } = require('path');
+const { spawnSync } = require('child_process');
 
 const npmCmd = (process.platform === 'win32') ? 'npm.cmd' : 'npm';
-const dataDir = path.join('test', 'data');
+const dataDir = join('test', 'data');
 const verbose = true; // turn this on if you want to see checker command output
 
-function checkProject(projectPath, install = true) {
-	const args = [path.join('bin', 'd2l-license-checker')];
+const checkProject = (projectPath, install = true) => {
+	const args = [join('bin', 'd2l-license-checker')];
+
 	if (projectPath) {
 		args.push(projectPath);
 	}
 
 	if (install) {
 		const npm = spawnSync(npmCmd, ['install'], { cwd: projectPath, shell: true });
+
 		if (npm.error !== undefined) {
 			throw npm.error;
 		}
+
 		if (verbose || npm.status !== 0) {
 			console.log('Running npm install');
 			console.log(`stdout:\n${npm.stdout}`);
@@ -26,85 +28,98 @@ function checkProject(projectPath, install = true) {
 
 		if (npm.status !== 0) {
 			console.error(npm);
+
 			throw new Error(`npm install failed (code: ${npm.status})`);
 		}
 	}
 
 	const node = spawnSync('node', args);
+
 	if (node.error !== undefined) {
 		throw node.error;
 	}
+
 	if (verbose) {
-		console.log(`Running command "node ${_.join(args, ' ')}"`);
+		console.log(`Running command "node ${args.join(' ')}"`);
 		console.log(`stdout:\n${node.stdout}`);
 		console.log(`stderr:\n${node.stderr}`);
 	}
 
 	return node.status;
-}
+};
 
-const makeTestPath = (proj) => path.join(dataDir, proj);
+const makeTestPath = (proj) => join(dataDir, proj);
 
-describe('Command invocation', function() {
-	this.timeout(12000);
+describe('license checker', () => {
+	describe('config', () => {
+		it('allows no config file', () => {
+			expect(checkProject(makeTestPath('proj-no-cfg'))).to.equal(0);
+		});
 
-	it('should accept a project with no dependencies', () => {
-		assert.equal(checkProject(makeTestPath('proj-no-deps')), 0);
+		it('rejects bad config', () => {
+			expect(checkProject(makeTestPath('proj-bad-cfg'))).to.equal(1);
+		});
 	});
 
-	it('should allow a project with no configuration file', () => {
-		assert.equal(checkProject(makeTestPath('proj-no-cfg')), 0);
+	describe('dependencies', () => {
+		it('accepts none', () => {
+			expect(checkProject(makeTestPath('proj-no-deps'))).to.equal(0);
+		});
+
+		it('ignores dev', () => {
+			expect(checkProject(makeTestPath('proj-ok-dev'))).to.equal(0);
+		});
+
+		it('ignores prod', () => {
+			expect(checkProject(makeTestPath('proj-ok-prod'))).to.equal(0);
+		});
 	});
 
-	it('should ignore dev dependencies', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-dev')), 0);
+	describe('licenses', () => {
+		it('rejects (dev)', () => {
+			expect(checkProject(makeTestPath('proj-license-issue-dev'))).to.equal(2);
+		});
+
+		it('rejects (prod)', () => {
+			expect(checkProject(makeTestPath('proj-license-issue'))).to.equal(2);
+		});
 	});
 
-	it('should ignore prod dependencies', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-prod')), 0);
+	describe('scopes', () => {
+		it('accepts d2l', () => {
+			expect(checkProject(makeTestPath('proj-ok-scope-d2l'), false)).to.equal(0);
+		});
+
+		it('rejects non-whitelisted', () => {
+			expect(checkProject(makeTestPath('proj-license-issue-scope'))).to.equal(2);
+		});
 	});
 
-	it('should reject bad config', () => {
-		assert.equal(checkProject(makeTestPath('proj-bad-cfg')), 1);
+	describe('overrides', () => {
+		it('accepts in-range', () => {
+			expect(checkProject(makeTestPath('proj-ok-range'))).to.equal(0);
+		});
+
+		it('rejects out-of-range', () => {
+			expect(checkProject(makeTestPath('proj-bad-override-range'))).to.equal(2);
+		});
+
+		it('allows special exemption', () => {
+			expect(checkProject(makeTestPath('proj-ok-override-special'))).to.equal(0);
+		});
 	});
 
-	it('should reject license (dev)', () => {
-		assert.equal(checkProject(makeTestPath('proj-license-issue-dev')), 2);
-	});
+	describe('SPDX expressions', () => {
+		it('allows valid', () => {
+			expect(checkProject(makeTestPath('proj-ok-spdx-expression'))).to.equal(0);
+		});
 
-	it('should reject license (prod)', () => {
-		assert.equal(checkProject(makeTestPath('proj-license-issue')), 2);
-	});
-
-	it('should accept the d2l scope', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-scope-d2l'), false), 0);
-	});
-
-	it('should reject a non-whitelisted scope', () => {
-		assert.equal(checkProject(makeTestPath('proj-license-issue-scope')), 2);
-	});
-
-	it('should accept an in-range override', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-range')), 0);
-	});
-
-	it('should reject an out-of-range override', () => {
-		assert.equal(checkProject(makeTestPath('proj-bad-override-range')), 2);
-	});
-
-	it('should allow a special exemption override', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-override-special')), 0);
-	});
-
-	it('should allow valid SPDX expression', () => {
-		assert.equal(checkProject(makeTestPath('proj-ok-spdx-expression')), 0);
-	});
-
-	it('should reject invalid SPDX expression', () => {
-		assert.equal(checkProject(makeTestPath('proj-bad-spdx-expression')), 2);
+		it('rejects invalid', () => {
+			expect(checkProject(makeTestPath('proj-bad-spdx-expression'))).to.equal(2);
+		});
 	});
 
 	it('self test', () => {
-		assert.equal(checkProject('.'), 0);
+		expect(checkProject('.')).to.equal(0);
 	});
 });
